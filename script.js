056c09912e2b32d30b0c6c44d7c283ec2b803531
akayukik <yuki-kuwahara@hotmail.com>
2024-06-06 20:10:44 -0700

    updates 6/6

---
+++ script.js
@@ -0,0 +1,6 @@
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
+
+
